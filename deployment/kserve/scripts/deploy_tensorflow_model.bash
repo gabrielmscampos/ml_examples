5893d40f87726aa603315017f74eaee1a858c224
Gabriel Moreira <gabrielmscampos@gmail.com>
2025-03-20 16:38:07 +0100

    feat: add script and template to deploy tensorflow inference service resources in kserve (targeting local minikube cluster)

---
+++ deployment/kserve/scripts/deploy_tensorflow_model.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./utils.bash
+
+tmp_kubeconfigs_path="../tmp_kube_configs"
+mkdir -p $tmp_kubeconfigs_path
+
+# Variables
+inputs_path="../inputs"
+templates_path="../templates"
+minio_aws_profile_name="local-minio"
+service_account_resource_name="s3-serviceaccount"
+minikube_ip=$(minikube ip)
+minio_api_node_port=$(kubectl get svc minio-service --namespace minio -o jsonpath='{.spec.ports[?(@.name=="api")].nodePort}')
+minio_external_api_url="http://$minikube_ip:$minio_api_node_port"
+
+# Deploy model and make it accessible
+service_name="tensorflow-example"
+host_model_path="../../../models/tensorflow/1"
+s3_model_dir="s3://test-bucket/tensorflow-example/1"
+storage_uri="\"s3://test-bucket/tensorflow-example\""
+aws s3 cp --recursive "$host_model_path" $s3_model_dir --profile $minio_aws_profile_name --endpoint-url=$minio_external_api_url &> /dev/null
+sed -e "s/{{ inference_service_resource_name }}/$service_name/g" \
+    -e "s/{{ service_account_resource_name }}/$service_account_resource_name/g" \
+    -e "s|{{ s3_model_root_path }}|$storage_uri|g" \
+    $templates_path/tensorflow.yaml \
+    > $tmp_kubeconfigs_path/tensorflow-isvc.yaml
+deploy_service "default" "$tmp_kubeconfigs_path/tensorflow-isvc.yaml" "$service_name"
+make_isvc_accessible "default" "$service_name"
+
+# Test predictions
+istio_node_port=$(kubectl get svc istio-ingressgateway --namespace istio-system -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+istio_base_url="http://$minikube_ip:$istio_node_port"
+model_name="tensorflow-example"
+service_name="tensorflow-example"
+namespace="default"
+url="${istio_base_url}/v1/models/${model_name}:predict"
+service_hostname=$(kubectl get inferenceservice ${service_name} --namespace "$namespace" -o jsonpath='{.status.url}' | cut -d "/" -f 3)
+input_path=@$inputs_path/tfserving_inputs.json
+curl -v -H "Host: ${service_hostname}" -H "Content-Type: application/json" $url -d $input_path
