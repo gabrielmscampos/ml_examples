0a8016b2d9534a2e41bc775e7aae60d03cb9b6f2
Gabriel Moreira <gabrielmscampos@gmail.com>
2025-03-26 14:38:33 +0100

    chore: add script ot test all deployments from scratch

---
+++ deployment/kserve/scripts/test_all_from_scratch.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./deploy_cluster.bash
+source ./deploy_s3_resources.bash
+source ./deploy_tensorflow_model.bash
+source ./deploy_pytorch_model.bash
+source ./deploy_xgboost_model.bash
+source ./deploy_lightgbm_model.bash
+source ./deploy_scikit_model.bash
